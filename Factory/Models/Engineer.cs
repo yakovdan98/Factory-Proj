f47985025a85415a038fd9b56ef7fed71ea5c82e
Daniel Yakovlev <Daniel.yakovlev@gmail.com>
2023-01-06 09:17:04 -0800

    created engineer

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace Factory.Models
+{
+  public class Engineer
+  {
+    public int EngineerId { get; set;}
+    public string Name { get; set; }
+    public List<EngineerMachine> JoinEntities { get; }
+
+  }
+}
