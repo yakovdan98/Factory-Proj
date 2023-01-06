1087922b3de5d167b164613b641e4b9bf9d5ccbb
Daniel Yakovlev <Daniel.yakovlev@gmail.com>
2023-01-06 10:36:42 -0800

    created machine

---
+++ Factory/Models/Machine.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace Factory.Models
+{
+  public class Machine
+  {
+    public int MachineId { get; set;}
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public List<EngineerMachine> JoinEntities { get; }
+    
+
+  }
+}
