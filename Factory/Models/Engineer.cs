2dab8d0f33be8a16a44c9a89b012b19d340fef28
Daniel Yakovlev <Daniel.yakovlev@gmail.com>
2023-01-06 15:42:24 -0800

    updated models to include validation

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Factory.Models
 {
   public class Engineer
   {
     public int EngineerId { get; set;}
+    [Required(ErrorMessage = "Engineer must have a name")]
     public string Name { get; set; }
     public List<EngineerMachine> JoinEntities { get; }
 
--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -1,11 +1,14 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Factory.Models
 {
   public class Machine
   {
     public int MachineId { get; set;}
+    [Required(ErrorMessage = "The machines's name can't be empty!")]
     public string Name { get; set; }
+    [Required(ErrorMessage = "The machines's description can't be empty!")]
     public string Description { get; set; }
     public List<EngineerMachine> JoinEntities { get; }
     
